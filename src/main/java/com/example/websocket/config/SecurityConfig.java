0fac0c187bd587fa2136529304de6723179a6c39
apekshashah029 <apeksha.shah@pirimidtech.com>
2026-01-20 15:02:14 +0530

    feat(refactor): Modularized google and github login seperately

--- src/main/java/com/example/websocket/config/SecurityConfig.java
+++ src/main/java/com/example/websocket/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
 import org.springframework.security.web.SecurityFilterChain;
@@ -25,6 +26,9 @@ public class SecurityConfig {
 
         http
                 .csrf(csrf->csrf.disable())
+//                .sessionManagement(session ->
+//                    session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+//                )
                 .authorizeHttpRequests(auth -> auth
                         .anyRequest().authenticated()
                 )
