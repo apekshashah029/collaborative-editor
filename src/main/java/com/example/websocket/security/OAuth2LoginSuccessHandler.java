e20a23970491ee996b3614eef5f0a088bce4cf0a
apekshashah029 <apeksha.shah@pirimidtech.com>
2026-01-20 15:02:14 +0530

    feat(refactor): Modularized google and github login seperately

--- src/main/java/com/example/websocket/security/OAuth2LoginSuccessHandler.java
+++ src/main/java/com/example/websocket/security/OAuth2LoginSuccessHandler.java
@@ -5,6 +5,8 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 
@@ -21,9 +23,35 @@ public class OAuth2LoginSuccessHandler implements AuthenticationSuccessHandler {
             Authentication authentication
     ) throws IOException, ServletException {
 
-        log.info("OAuth2 login successful");
-        log.info("Authenticated user: {}", authentication.getName());
+        OAuth2AuthenticationToken oauthToken = (OAuth2AuthenticationToken) authentication;
+
+        String provider = oauthToken.getAuthorizedClientRegistrationId(); // either google or github
+        OAuth2User oauthUser = oauthToken.getPrincipal();
+
+        log.info("OAuth2 login successful via {}", provider);
+
+        if ("google".equals(provider)) {
+            handleGoogleLogin(oauthUser);
+        } else if ("github".equals(provider)) {
+            handleGithubLogin(oauthUser);
+        }
 
         response.sendRedirect("/index.html");
+    }
+
+    private void handleGoogleLogin(OAuth2User user) {
+        String email = user.getAttribute("email");
+        String name = user.getAttribute("name");
+        String googleId = user.getAttribute("sub");
+
+        log.info("Google user -> email={}, name={}, id={}", email, name, googleId);
+    }
+
+    private void handleGithubLogin(OAuth2User user) {
+        String username = user.getAttribute("login");
+        String email = user.getAttribute("email"); // may be null if github haven't  attached email
+        Integer githubId = user.getAttribute("id");
+
+        log.info("GitHub user -> username={}, email={}, id={}", username, email, githubId);
     }
 }
