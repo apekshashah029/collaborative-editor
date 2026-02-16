e4551b4e81d0ddd427364d1f741021338c8cce99
apekshashah029 <apeksha.shah@pirimidtech.com>
2026-02-16 17:37:04 +0530

    refactor: remove unused method

--- src/main/java/com/example/websocket/mapper/UserMapper.java
+++ src/main/java/com/example/websocket/mapper/UserMapper.java
@@ -1,15 +1,11 @@
 package com.example.websocket.mapper;
 
 import com.example.websocket.dto.LoginRequestDTO;
-import com.example.websocket.dto.UserResponseDTO;
 import com.example.websocket.entity.User;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
 
     User toEntity(LoginRequestDTO dto);
-
-    UserResponseDTO toResponse(User user);
 }
